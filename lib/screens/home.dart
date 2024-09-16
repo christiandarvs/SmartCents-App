b82f5bcd898c422ab295ce6351b61ff0c013cb37
christiandarvs <christiandarvin4@gmail.com>
2024-09-16 10:13:37 +0800

    add bottom nav bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -20,6 +20,24 @@ class Home extends StatelessWidget {
             children: [Text('Hello')],
           ),
         ),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedFontSize: 0,
+          backgroundColor: AppColors.primaryColor,
+          selectedIconTheme: const IconThemeData(color: Colors.white),
+          unselectedIconTheme: const IconThemeData(color: Color(0xffFFD500)),
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home), label: 'Home', tooltip: 'Home'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.book), label: 'Courses', tooltip: 'Courses'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.account_circle_outlined),
+                label: 'Profile',
+                tooltip: 'Profile'),
+          ],
+          currentIndex: 0,
+          onTap: (value) {},
+        ),
       ),
     );
   }
